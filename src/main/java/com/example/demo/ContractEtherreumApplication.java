edacc81d860dfdf024f62aee741060c9f8ce3a26
BARROU Mouad <81191265+barrou-mouad@users.noreply.github.com>
2022-01-01 12:20:23 +0100

    first commit

---
+++ src/main/java/com/example/demo/ContractEtherreumApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractEtherreumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractEtherreumApplication.class, args);
+	}
+
+}
